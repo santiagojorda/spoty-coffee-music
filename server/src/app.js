3028b977a04a93886ca52bcac56fc4626596528f
santiagojorda <sjorda@fi.uba.ar>
2023-03-22 14:03:48 -0300

    add song to queoe

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,11 @@ devMessage('INITIALIZING SERVER')
 require('./config/env_vars_init')
 const {PORT, HOST} = process.env
 
+app.use('/', require('./routes/root'))
+
 app.listen(PORT, HOST, (req, res) => {
     devMessage(`SERVER INITIALIZED SUCCESSFULLY ON: ${HOST}:${PORT}`)
 })
+
 
 module.exports = app
